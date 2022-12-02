8ac5338943d299c5a6e88ef8f4752b57ba18658c
Naetan07 <oreofeogunrekun@gmail.com>
2022-12-02 23:38:04 +0100

    final commit done with everything damn|||

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,31 +10,38 @@ import pinterest from "../assets/images/icon-pinterest.svg"
 const Footer = () => {
     return (
         <footer className='footer'>
-            <div>
-                <div className='logo'>
-                    <img src={Logo} alt="" />
+            <div className='foot-flex'>
+                <div className= 'contact-flex'>
+                    <div className='logo'>
+                        <img src={Logo} alt="" />
+                    </div>
+                    <ul className='social'>
+                        <li><img src={Facebook} alt="" /></li>
+                         <li><img src={youtube} alt="" /></li>
+                         <li><img src={twitter} alt="" /></li>
+                          <li><img src={pinterest} alt="" /></li>
+                           <li><img src={instagram} alt="" /></li>
+                    </ul>
                 </div>
-                 <ul className='social'>
-                    <li><img src={Facebook} alt="" /></li>
-                     <li><img src={youtube} alt="" /></li>
-                     <li><img src={twitter} alt="" /></li>
-                      <li><img src={pinterest} alt="" /></li>
-                       <li><img src={instagram} alt="" /></li>
-                </ul>
-                <ul>
-                    <li>About</li>
-                    <li>contact</li>
-                    <li>Blog</li>
-                    <li>Careers</li>
-                    <li>Support</li>
-                    <li>Privacy Policy</li>
-
-                </ul>
-                <button>Request Invite</button>
-            </div>
-                <div className='copy-right'>
-                   © Easybank.. All Right Reserved.
+                <div className='info-flex'>
+                    <ul>
+                        <li>About</li>
+                        <li>contact</li>
+                        <li>Blog</li>
+                    </ul>    
+                    <ul>
+                        <li>Careers</li>
+                        <li>Support</li>
+                        <li>Privacy Policy</li>
+                    </ul>
                 </div>
+                <div className= 'last-flex'>
+                    <button>Request Invite</button>
+                    <div className='copy-right'>
+                       © Easybank.. All Right Reserved.
+                    </div>
+                </div>
+            </div> 
         </footer>
     )
 }
